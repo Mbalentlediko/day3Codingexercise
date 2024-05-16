618b6c151544f002f5fab3b56a53debd972ad907
Mbalentlediko <dikombalentle9@gmail.com>
2024-05-16 14:13:47 +0200

    Add Answers

---
+++ script/code.js
@@ -0,0 +1,12 @@
+let lastName = 'Diko'
+console.log(typeof lastName);
+
+lastName= 2
+console.log(typeof lastName);
+// Yes the value changed to a number
+
+let Status = true
+console.log(typeof Status);
+
+let nothing = null
+console.log(typeof nothing);
